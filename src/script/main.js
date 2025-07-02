22822e1c896e6f7606365a54fcffcedd2f44869a
JoannEmawodia <emawodia@gmail.com>
2025-07-02 09:44:35 -0300

    finalizando a estilização da pagina

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,9 @@
-console.log('olá, mundo');
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+},1000);
